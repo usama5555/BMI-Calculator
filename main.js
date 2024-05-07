3db33d4e92b7f76c62cc7aff3864c619b16723ed
usama5555 <usamamaqsood0007@gmail.com>
2024-05-07 18:39:50 +0500

    Make a BMI Calculator

---
+++ main.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function (e){
+    e.preventDefault()
+    const height = parseInt(document.querySelector('#height').value)
+    const weight =  parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#result')
+
+    if (height === "" || height < 0 || isNaN(height)){
+        result.innerHTML = `Please give a valid Height ${height}`
+    }
+    
+   else if (weight === "" || weight < 0 || isNaN(weight)){
+        result.innerHTML = `Please give a valid weight ${weight}`
+    }
+    else{
+        const bmi = (weight/((height*height)/10000)).toFixed(2)
+        // result.innerHTML = `<span>${bmi}</span>`
+
+        if(bmi < 18.6){
+            result.innerHTML=`<span>${bmi}<br> Underweight</span>`
+        }
+        else if(bmi > 18.6 && bmi <= 24.9){
+            result.innerHTML=`<span>${bmi}<br> Normal</span>`
+        }
+        else{
+            result.innerHTML=`<span>${bmi}<br> Overweight</span>`
+        }
+        
+    }
+    
+})
